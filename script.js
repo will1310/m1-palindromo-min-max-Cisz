840d9f268577ccb396454bddbc6382a7352e0ce1
Cisz <cisz13h@gmail.com>
2024-10-23 08:54:00 -0300

    tarefa finalizado

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-function isPalindrome(str){
-    /* Seu código aqui */
-}
+function isPalindrome(str) {
+    for (let i = 0; i < str.length; i++) {
+        palavraNormal = str[i].toLowerCase()
+    }
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
-}
+    let indiciInicial = str.length - 1;
+    for (let index = indiciInicial; index >= 0 ; index--){
+    palavraInversa = str[index].toLowerCase()
+    }
+
+    if (palavraInversa == palavraNormal){
+        return true
+    } else return false
+
+  }
+
+  function arrayMaxMin (numeros){
+    
+    let result = [];
+    let numeroMaisLongo = numeros[0]
+    let numeroMaisCurto = numeros[0]    
+     
+    for(let i = 0; i < numeros.length ; i++){
+        if(numeros[i] > numeroMaisLongo){
+            numeroMaisLongo = numeros[i]
+        }
+        if(numeros[i] < numeroMaisCurto){
+            numeroMaisCurto = numeros[i]
+        }
+        result = [numeroMaisCurto, numeroMaisLongo]
+
+    }
+
+    return result
+  }
